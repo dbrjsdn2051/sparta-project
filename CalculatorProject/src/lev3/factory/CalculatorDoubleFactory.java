518fb9b6b4f14687bf1e02516aa884c02b879109
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-09-04 17:21:27 +0900

    에러 처리 수정

--- CalculatorProject/src/lev3/factory/CalculatorDoubleFactory.java
+++ CalculatorProject/src/lev3/factory/CalculatorDoubleFactory.java
@@ -26,7 +26,7 @@ public enum CalculatorDoubleFactory {
             if(value.operator.equals(operator)) return value.abstractOperation;
         }
 
-        throw new Exception("연산자 타입이 아닙니다.");
+        return null;
     }
 
 }
