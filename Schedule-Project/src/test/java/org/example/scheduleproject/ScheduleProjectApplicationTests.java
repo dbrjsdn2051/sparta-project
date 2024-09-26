4d6554c795218a0ae89fbba8a0bad36a74467715
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-09-26 11:00:08 +0900

    첫번째 커밋

---
+++ Schedule-Project/src/test/java/org/example/scheduleproject/ScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scheduleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
