56638977a8782975903d001d66b2a33475963795
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-08 21:03:46 +0900

    init

---
+++ TodoList-Project/src/test/java/org/example/todolistproject/TodoListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todolistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
