38d9803b7fd0f21f3e397ee058ca19cf3ba8f724
dbrjsdn2051 <149162352+dbrjsdn2051@users.noreply.github.com>
2024-10-08 21:03:04 +0900

    Delete TodoList-Project/src directory

--- TodoList-Project/src/main/java/org/example/todolistproject/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package org.example.todolistproject.entity;
-
-public enum Role {
-    USER, ADMIN
-}
--- TodoList-Project/src/main/java/org/example/todolistproject/exception/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package org.example.todolistproject.exception;
-
-import jakarta.annotation.Nullable;
-
-public class AuthorizationException extends RuntimeException{
-    public AuthorizationException() {
-        super("허가된 접근이 아닙니다!!");
-    }
-}
