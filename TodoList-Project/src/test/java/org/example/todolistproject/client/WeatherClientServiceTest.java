58d6ccc7155d8e737902ecce64d5c81a7e495813
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-16 16:36:14 +0900

    테스트 코드 업로드

---
+++ TodoList-Project/src/test/java/org/example/todolistproject/client/WeatherClientServiceTest.java
@@ -0,0 +1,84 @@
+package org.example.todolistproject.client;
+
+import org.example.todolistproject.client.dto.WeatherResponseDto;
+import org.example.todolistproject.exception.NoResultDataException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherClientServiceTest {
+
+    @Mock
+    WeatherClient weatherClient;
+
+    @InjectMocks
+    WeatherClientService weatherClientService;
+
+    private LocalDateTime today;
+
+    @BeforeEach
+    void setUp() {
+        today = LocalDateTime.now();
+    }
+
+    @Test
+    void getWeatherSuccessTest() {
+        // given
+        String formatDate = today.format(DateTimeFormatter.ofPattern("MM-dd"));
+        WeatherResponseDto dto1 = new WeatherResponseDto("01-01", "Sunny");
+        WeatherResponseDto dto2 = new WeatherResponseDto(formatDate, "Rainy");
+        List<WeatherResponseDto> weatherList = Arrays.asList(dto1, dto2);
+
+        when(weatherClient.getWeather()).thenReturn(weatherList);
+
+        //when
+        String weather = weatherClientService.getWeather(today);
+
+        //then
+        assertThat(weather).isEqualTo("Rainy");
+        verify(weatherClient, times(1)).getWeather();
+    }
+
+    @Test
+    void getWeatherFailTest() {
+        // given
+        String formatDate = today.format(DateTimeFormatter.ofPattern("MM-dd"));
+        WeatherResponseDto dto1 = new WeatherResponseDto("01-01", "Sunny");
+        WeatherResponseDto dto2 = new WeatherResponseDto("02-02", "Rainy");
+        List<WeatherResponseDto> weatherList = Arrays.asList(dto1, dto2);
+
+        when(weatherClient.getWeather()).thenReturn(weatherList);
+
+        //when & then
+        assertThrows(NoResultDataException.class, () -> weatherClientService.getWeather(today));
+
+        verify(weatherClient, times(1)).getWeather();
+    }
+
+    @Test
+    void getWeatherEmptyTest() {
+        //given
+        when(weatherClient.getWeather()).thenReturn(Collections.emptyList());
+
+        // when & then
+        assertThrows(NoResultDataException.class, () -> weatherClientService.getWeather(today));
+
+        verify(weatherClient, times(1)).getWeather();
+    }
+
+
+}
---
+++ TodoList-Project/src/test/java/org/example/todolistproject/factory/ScheduleFactoryTest.java
@@ -0,0 +1,139 @@
+package org.example.todolistproject.factory;
+
+import org.example.todolistproject.client.WeatherClientService;
+import org.example.todolistproject.config.PasswordEncoder;
+import org.example.todolistproject.dto.schedule.ScheduleDto;
+import org.example.todolistproject.entity.Role;
+import org.example.todolistproject.entity.Schedule;
+import org.example.todolistproject.entity.User;
+import org.example.todolistproject.exception.MissMatchPasswordException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+//@Import(WeatherClientService.class)
+class ScheduleFactoryTest {
+
+    @Mock
+    PasswordEncoder passwordEncoder;
+
+    @Mock
+    WeatherClientService weatherClientService;
+
+    @InjectMocks
+    ScheduleFactory scheduleFactory;
+
+    ScheduleDto.Create createDto;
+    ScheduleDto.Update updateDto;
+    ScheduleDto.Delete deleteDto;
+    Schedule schedule ;
+
+
+    @BeforeEach
+    void init() {
+        createDto = new ScheduleDto.Create("title1", "content", "1234", "sunny");
+
+        updateDto = new ScheduleDto.Update();
+        updateDto.setScheduleId(1L);
+        updateDto.setPassword("1234");
+        updateDto.setContent("content1234");
+
+        deleteDto = new ScheduleDto.Delete();
+        deleteDto.setScheduleId(1L);
+        deleteDto.setPassword("1234");
+
+        User user = User.builder()
+                .username("testUser")
+                .password("encodedPassword")
+                .role(Role.USER)
+                .email("user@naver.com")
+                .build();
+
+
+        schedule = Schedule.builder()
+                .title("Main Title1")
+                .content("TIL")
+                .weather("rain")
+                .password("encodedPassword")
+                .user(user)
+                .build();
+    }
+
+    @Test
+    void createSuccessTest(){
+
+        //given
+        String password = createDto.getPassword();
+        String encodedPassword = "securedPassword";
+        when(passwordEncoder.encode(password)).thenReturn(encodedPassword);
+        when(weatherClientService.getWeather(any())).thenReturn("Sunny");
+
+        // when
+        Schedule createSchedule = scheduleFactory.createSchedule(createDto, schedule.getUser());
+
+        // then
+        assertThat(createSchedule.getContent()).isEqualTo(createDto.getContent());
+        assertThat(createSchedule.getTitle()).isEqualTo(createDto.getTitle());
+        assertThat(createSchedule.getWeather()).isEqualTo("Sunny");
+        assertThat(createSchedule.getPassword()).isEqualTo(encodedPassword);
+
+        verify(passwordEncoder, times(1)).encode(any());
+    }
+
+    @Test
+    void updateSuccessTest(){
+        // given
+
+        // when
+        scheduleFactory.updateSchedule(updateDto, schedule);
+
+        // then
+        assertThat(schedule.getContent()).isEqualTo(updateDto.getContent());
+        verify(passwordEncoder, times(1)).validPassword(any(), any());
+    }
+
+
+    @Test
+    void updateFailTest(){
+        // given
+        doThrow(MissMatchPasswordException.class)
+                .when(passwordEncoder)
+                .validPassword(updateDto.getPassword(), schedule.getPassword());
+        // when & then
+        assertThrows(MissMatchPasswordException.class, () -> scheduleFactory.updateSchedule(updateDto, schedule));
+    }
+
+    @Test
+    void deleteSuccessTest(){
+        // given
+
+        // when
+        scheduleFactory.deleteSchedule(deleteDto, schedule);
+
+        // then
+        verify(passwordEncoder, times(1)).validPassword(any(), any());
+    }
+
+    @Test
+    void deleteFailTest(){
+        // given
+        doThrow(MissMatchPasswordException.class)
+                .when(passwordEncoder)
+                .validPassword(deleteDto.getPassword(), schedule.getPassword());
+
+        // when & then
+        assertThrows(MissMatchPasswordException.class,
+                () -> scheduleFactory.deleteSchedule(deleteDto, schedule));
+    }
+
+
+}
